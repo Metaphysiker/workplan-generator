74fc78556f4b8acfd4edad65af4d6a57915d0d17
Metaphysiker <s.raess@me.com>
2023-07-29 15:51:03 +0200

    welcome route

--- ember-app/app/app.js
+++ ember-app/app/app.js
@@ -2,6 +2,8 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-app/config/environment';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
--- ember-app/app/routes/scientists.js
+++ ember-app/app/routes/scientists.js
@@ -1,13 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class ScientistsRoute extends Route {
-    model() {
-        let scientists = ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
-        let swissPeople = ["Ueli", "Ferdinand"];
-
-      return {
-        scientists: scientists,
-        swissPeople: swissPeople
-      };
-    }
-  }
+export default class ScientistsRoute extends Route {}
